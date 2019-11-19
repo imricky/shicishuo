dac29c942a294b7a359b08bdbe51a95749c572ef
rqcao <rqcao@outlook.com>
2019-11-19 11:25:37 +0800

    引入 element

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -8,8 +8,15 @@ module.exports = {
     '@vue/airbnb',
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    //'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    //'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    //"off"或者0    //关闭规则关闭
+    //"warn"或者1    //在打开的规则作为警告（不影响退出代码）
+    //"error"或者2    //把规则作为一个错误（退出代码触发时为1）
+    "no-var": 2,//禁用var，用let和const代替
+    "no-use-before-define": 2,//未定义前不能使用
+    "semi": [2, "always"],//语句强制分号结尾
+    "indent": [2],//缩进风格
   },
   parserOptions: {
     parser: 'babel-eslint',
--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+// eslint-disable-next-line import/extensions
+import './plugins/element.js';
 
 Vue.config.productionTip = false;
 
