22dc72107d09da6654e5f31bea426afd9f26cc9d
rqcao <rqcao@outlook.com>
2019-11-21 21:18:41 +0800

    添加Library（文库大全）页面

---
+++ client/src/components/Library.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="library">
+    <el-container>
+      <el-aside class="aside-container">
+        123
+      </el-aside>
+      <el-main class="main-container">
+        123
+
+        <el-divider></el-divider>
+        <el-pagination
+          background
+          layout="prev, pager, next"
+          :total="1000">
+        </el-pagination>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Library',
+  data() {
+    return {
+    };
+  },
+  computed: {},
+  methods: {},
+  created() {
+
+  },
+  mounted() {
+
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  .library{
+    max-width: 1000px;
+    border: 1px solid rebeccapurple;
+    background-color: #E9EEF3;
+    color: #333;
+    /*margin: 10px 20px;*/
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto;
+  }
+  .aside-container{
+    width: 200px;
+    border: 1px solid #E6A23C;
+  }
+  .main-container{
+    border: 1px solid #409EFF;
+    margin-left: 20px;
+  }
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,26 +1,29 @@
 <template>
   <div class="home">
-    <TopBar />
+<!--    <TopBar />-->
 <!--    <DailyPoem />-->
-    <ExploreGoodPoetry />
-    <Footer />
+<!--    <ExploreGoodPoetry />-->
+    <library />
+<!--    <Footer />-->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import TopBar from '@/components/TopBar.vue';
+// import TopBar from '@/components/TopBar.vue';
 // import DailyPoem from '@/components/DailyPoem.vue';
-import ExploreGoodPoetry from '@/components/ExploreGoodPoetry.vue';
-import Footer from '@/components/Footer.vue';
+// import ExploreGoodPoetry from '@/components/ExploreGoodPoetry.vue';
+// import Footer from '@/components/Footer.vue';
+import Library from '@/components/Library.vue';
 
 export default {
   name: 'home',
   components: {
-    TopBar,
+    // TopBar,
     // DailyPoem,
-    ExploreGoodPoetry,
-    Footer,
+    // ExploreGoodPoetry,
+    Library,
+    // Footer,
   },
 };
 </script>
