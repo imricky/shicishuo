f4228f5412365e42ec79569cfbe9d7012a5ddbe2
rqcao <rqcao@outlook.com>
2019-11-21 21:50:07 +0800

    基本完成Library（文库大全）页面的结构，还没有调试样式

--- client/src/components/Library.vue
+++ client/src/components/Library.vue
@@ -2,10 +2,96 @@
   <div class="library">
     <el-container>
       <el-aside class="aside-container">
-        123
+        <el-card class="one-card website-info">
+          <div slot="header">
+            <span>网站信息</span>
+          </div>
+          <div>
+            <p class="all-poetry">本网站共收录：<span>20000</span>首诗词</p>
+            <p>本网站共收录：<span>3000</span>位诗人</p>
+          </div>
+        </el-card>
+        <el-card class="one-card top-ten-poet">
+          <div slot="header">
+            <span>十大高产诗人</span>
+          </div>
+          <div>
+            <el-table
+              :data="tableData"
+              style="width: 100%">
+              <el-table-column type="expand">
+                <template slot-scope="props">
+                  <el-form label-position="left" inline class="demo-table-expand">
+                    <el-form-item label="商品名称">
+                      <span>{{ props.row.name }}</span>
+                    </el-form-item>
+                    <el-form-item label="所属店铺">
+                      <span>{{ props.row.shop }}</span>
+                    </el-form-item>
+                    <el-form-item label="商品 ID">
+                      <span>{{ props.row.id }}</span>
+                    </el-form-item>
+                    <el-form-item label="店铺 ID">
+                      <span>{{ props.row.shopId }}</span>
+                    </el-form-item>
+                    <el-form-item label="商品分类">
+                      <span>{{ props.row.category }}</span>
+                    </el-form-item>
+                    <el-form-item label="店铺地址">
+                      <span>{{ props.row.address }}</span>
+                    </el-form-item>
+                    <el-form-item label="商品描述">
+                      <span>{{ props.row.desc }}</span>
+                    </el-form-item>
+                  </el-form>
+                </template>
+              </el-table-column>
+              <el-table-column
+                label="商品 ID"
+                prop="id">
+              </el-table-column>
+              <el-table-column
+                label="商品名称"
+                prop="name">
+              </el-table-column>
+            </el-table>
+          </div>
+        </el-card>
+        <el-card class="one-card popular-words">
+          <div slot="header">
+            <span>热门词语</span>
+          </div>
+          <div>
+            <el-tag
+              v-for="item in items"
+              :key="item.label"
+              :type="item.type"
+              effect="plain">
+              {{ item.label }}
+            </el-tag>
+          </div>
+        </el-card>
       </el-aside>
       <el-main class="main-container">
-        123
+        <el-table
+          :data="tableData1"
+          stripe
+          style="width: 100%">
+          <el-table-column
+            prop="date"
+            label="日期"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="name"
+            label="姓名"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="address"
+            label="地址">
+          </el-table-column>
+        </el-table>
 
         <el-divider></el-divider>
         <el-pagination
@@ -23,6 +109,63 @@ export default {
   name: 'Library',
   data() {
     return {
+      tableData: [{
+        id: '12987122',
+        name: '好滋好味鸡蛋仔',
+        category: '江浙小吃、小吃零食',
+        desc: '荷兰优质淡奶，奶香浓而不腻',
+        address: '上海市普陀区真北路',
+        shop: '王小虎夫妻店',
+        shopId: '10333',
+      }, {
+        id: '12987123',
+        name: '好滋好味鸡蛋仔',
+        category: '江浙小吃、小吃零食',
+        desc: '荷兰优质淡奶，奶香浓而不腻',
+        address: '上海市普陀区真北路',
+        shop: '王小虎夫妻店',
+        shopId: '10333',
+      }, {
+        id: '12987125',
+        name: '好滋好味鸡蛋仔',
+        category: '江浙小吃、小吃零食',
+        desc: '荷兰优质淡奶，奶香浓而不腻',
+        address: '上海市普陀区真北路',
+        shop: '王小虎夫妻店',
+        shopId: '10333',
+      }, {
+        id: '12987126',
+        name: '好滋好味鸡蛋仔',
+        category: '江浙小吃、小吃零食',
+        desc: '荷兰优质淡奶，奶香浓而不腻',
+        address: '上海市普陀区真北路',
+        shop: '王小虎夫妻店',
+        shopId: '10333',
+      }],
+      items: [
+        { type: '', label: '标签一' },
+        { type: 'success', label: '标签二' },
+        { type: 'info', label: '标签三' },
+        { type: 'danger', label: '标签四' },
+        { type: 'warning', label: '标签五' },
+      ],
+      tableData1: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄',
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄',
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄',
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄',
+      }],
     };
   },
   computed: {},
@@ -51,6 +194,12 @@ export default {
   .aside-container{
     width: 200px;
     border: 1px solid #E6A23C;
+    .one-card{
+      margin-bottom: 20px;
+    }
+    .all-poetry{
+      border-bottom: 1px solid gray;
+    }
   }
   .main-container{
     border: 1px solid #409EFF;
