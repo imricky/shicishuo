c0b4c5de8ab8c3049961594b62c597e31152c931
rqcao <rqcao@outlook.com>
2019-12-15 17:46:23 +0800

    统一样式更新一下

--- client/src/App.vue
+++ client/src/App.vue
@@ -17,11 +17,30 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+  /*-webkit-font-smoothing: antialiased;*/
+  /*-moz-osx-font-smoothing: grayscale;*/
+  /*width: 100%;*/
+  /*height: 100%;*/
   /*text-align: center;*/
   /*color: #2c3e50;*/
   /*margin-top: 60px;*/
+}
+
+body,html {
+  /*color: #000;*/
+  height: 100%;
+}
+#app {
+  text-align: center;
+  height: 100%;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+.clearfix::after{
+  content: '';
+  display: block;
+  clear: both;
 }
 </style>
