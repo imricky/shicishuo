dea658370b06635a504e82cf4c319f4312fd55f0
rqcao <rqcao@outlook.com>
2020-02-05 23:17:53 +0800

    注册页面 移动端适配

--- client/src/views/user/Register.vue
+++ client/src/views/user/Register.vue
@@ -3,9 +3,9 @@
   <div class="register-wrap">
     <div class="register-main">
       <div class="logo">
-        <p class="logo-word">
-          SHICISHUO  -  register
-        </p>
+        <h2 class="logo-word">
+          诗词说  -  注册
+        </h2>
       </div>
       <div class="main">
         <el-form label-position="top" label-width="80px" :model="registerForm" :rules="rules" ref="registerForm">
@@ -24,7 +24,7 @@
         </el-form>
       </div>
       <div class="register-bottom">
-        <el-button type="primary" class="register-button" @click="doRegister('registerForm')">注册</el-button>
+        <el-button type="primary" class="register-button register-bottom-common" @click="doRegister('registerForm')">注册</el-button>
         <el-link class="already-have-username" @click="goLogin">已有账号？登录</el-link>
       </div>
     </div>
@@ -160,44 +160,99 @@ export default {
     background-size: 100% 100%;
     position: relative;
   }
-  .register-main{
-    width: 500px;
-    /*height: 400px;*/
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    border: 1px solid #606266;
-    transform: translate(-50%,-50%);
-    box-shadow: #666 0 0 30px;
-  }
-  .logo{
-    border: 1px solid #E6A23C;
-    height: 50px;
-    margin: 20px;
-    font-size: 24px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    .logo-word{
-      color: #00B7FF;
+
+  /* 手机以外的分辨率 */
+  @media screen and (min-width: 481px) {
+    .register-main{
+      width: 500px;
+      /*height: 400px;*/
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      border: 1px solid #606266;
+      transform: translate(-50%,-50%);
+      box-shadow: #666 0 0 30px;
+    }
+    .logo{
+      border: 1px solid #E6A23C;
+      height: 50px;
+      margin: 20px;
+      font-size: 24px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .logo-word{
+        color: #303133;
+      }
+    }
+
+    /*输入框区域*/
+    .main{
+      margin: 20px;
+    }
+
+    /*按钮区域*/
+    .register-bottom{
+      display: flex;
+      flex-direction: column; //垂直对齐
+      justify-content: center;
+      align-items: center;
+      margin: 20px 40px;
+      .already-have-username{
+        margin-top: 10px;
+      }
     }
   }
 
-  /*输入框区域*/
-  .main{
-    margin: 20px;
+  /* 横向放置的手机和竖向放置的平板之间的分辨率 */
+  @media screen and (max-width: 767px) {
+
   }
 
-  /*按钮区域*/
-  .register-bottom{
-    display: flex;
-    flex-direction: column; //垂直对齐
-    justify-content: center;
-    align-items: center;
-    margin: 20px 40px;
-    .already-have-username{
-      margin-top: 10px;
+  /* 横向放置的手机及分辨率更小的设备 */
+  @media screen and (max-width: 480px) {
+    .logo{
+      /*border: 1px solid red;*/
+      position: absolute;
+      top: 20%;
+      transform: translateY(-50%);
+      margin: 0 auto;
+      width: 100%;
+      text-align: center;
     }
-  }
 
+
+    .mobile-logo-word{
+    }
+    .main{
+      /*border: 1px solid red;*/
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      width: 100%;
+      margin: 0 auto;
+      padding: 0 20px;
+    }
+    .register-bottom{
+      /*border: 1px solid red;*/
+      position: absolute;
+      top: 75%;
+      transform: translateY(-50%);
+      width: 100%;
+      margin: 0 auto;
+      text-align: center;
+      margin-top: 40px;
+    }
+    .mobile-login-button{
+      display: block;
+      margin: 0 auto;
+      margin-bottom: 10px;
+    }
+    .register-bottom-common{
+      display: block;
+      margin: 0 auto;
+      margin-bottom: 10px;
+    }
+
+  }
 </style>
