78ac953fb75d0c6ee5edeae5bae8268e1da1be7f
rqcao <rqcao@outlook.com>
2020-02-05 22:58:49 +0800

    登录界面移动端适配

--- client/src/views/user/Login.vue
+++ client/src/views/user/Login.vue
@@ -1,28 +1,50 @@
 <template>
 <!--  TODO： 增加背景图-->
-  <div class="login-wrap">
-    <div class="login-main">
-      <div class="logo">
-        <p class="logo-word">
-          SHICISHUO  -  login
-        </p>
+    <div class="login-wrap">
+      <div class="login-main">
+        <div class="logo">
+          <p class="logo-word">
+            SHICISHUO  -  login
+          </p>
+        </div>
+        <div class="main">
+          <el-form label-position="top" label-width="80px" :model="loginForm" :rules="rules" ref="loginForm">
+            <el-form-item label="用户名" prop="username">
+              <el-input v-model="loginForm.username"></el-input>
+            </el-form-item>
+            <el-form-item label="密码" prop="password">
+              <el-input type="password" v-model="loginForm.password"></el-input>
+            </el-form-item>
+          </el-form>
+        </div>
+        <div class="login-bottom">
+          <el-button type="primary" @click="doLogin('loginForm')">登录</el-button>
+          <el-link href="/t" target="_blank"  class="forgot">我忘记密码了</el-link>
+        </div>
       </div>
-      <div class="main">
-        <el-form label-position="top" label-width="80px" :model="loginForm" :rules="rules" ref="loginForm">
-          <el-form-item label="用户名" prop="username">
-            <el-input v-model="loginForm.username"></el-input>
-          </el-form-item>
-          <el-form-item label="密码" prop="password">
-            <el-input type="password" v-model="loginForm.password"></el-input>
-          </el-form-item>
-        </el-form>
-      </div>
-      <div class="login-bottom">
-        <el-button type="primary" class="login-button" @click="doLogin('loginForm')">登录</el-button>
-        <el-link href="/t" target="_blank"  class="forgot">我忘记密码了</el-link>
-      </div>
+      <div class="mobile-all">
+            <div class="mobile-title">
+              <h1 class="mobile-logo-word">
+                SHICISHUO  -  login
+              </h1>
+            </div>
+            <div class="mobile-main">
+              <el-form label-position="top" label-width="80px" :model="loginForm" :rules="rules" ref="loginForm">
+                <el-form-item label="用户名" prop="username">
+                  <el-input v-model="loginForm.username"></el-input>
+                </el-form-item>
+                <el-form-item label="密码" prop="password">
+                  <el-input type="password" v-model="loginForm.password"></el-input>
+                </el-form-item>
+              </el-form>
+            </div>
+            <div class="mobile-bottom">
+              <el-button type="primary" class="mobile-login-button" @click="doLogin('loginForm')">登录</el-button>
+              <el-link href="/t" target="_blank"  class="mobile-forget">我忘记密码了</el-link>
+            </div>
+          </div>
     </div>
-  </div>
+
 </template>
 
 <script>
@@ -149,6 +171,73 @@ export default {
     .forgot{
       margin-top: 10px;
     }
+  }
+
+  /*移动端显示*/
+  .mobile-all{
+    display: none;
+  }
+
+  /* 平板电脑和小屏电脑之间的分辨率 */
+  @media screen and (min-width: 768px) and (max-width: 979px) {
+  }
+
+  /* 横向放置的手机和竖向放置的平板之间的分辨率 */
+  @media screen and (max-width: 767px) {
+
+  }
+
+  /* 横向放置的手机及分辨率更小的设备 */
+  @media screen and (max-width: 480px) {
+    .login-main{
+      display: none;
+    }
+
+    .mobile-all{
+      display: block;
+    }
+    .mobile-title{
+      /*border: 1px solid red;*/
+      position: absolute;
+      top: 25%;
+      transform: translateY(-50%);
+      margin: 0 auto;
+      width: 100%;
+      text-align: center;
+    }
+
+    .mobile-logo-word{
+    }
+    .mobile-main{
+      /*border: 1px solid red;*/
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      width: 100%;
+      margin: 0 auto;
+      padding: 0 20px;
+    }
+    .mobile-bottom{
+      /*border: 1px solid red;*/
+      position: absolute;
+      top: 70%;
+      transform: translateY(-50%);
+      width: 100%;
+      margin: 0 auto;
+      text-align: center;
+      margin-top: 20px;
+    }
+    .mobile-login-button{
+      display: block;
+      margin: 0 auto;
+      margin-bottom: 10px;
+    }
+    .mobile-forget{
+      display: block;
+      margin: 0 auto;
+      margin-bottom: 10px;
+    }
+
   }
 
 </style>
