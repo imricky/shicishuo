a2442b75b0ec31d5c5372d21223bf73090df0f54
rqcao <rqcao@outlook.com>
2020-02-13 23:37:11 +0800

    开始写加入房间的功能

--- server/utils/socketio.js
+++ server/utils/socketio.js
@@ -10,10 +10,41 @@ io.on('connection', (socket) => {
     io.emit('test1', `${msg}1你好啊`);
   });
 
+  // 同步绘画面板图案
   socket.on('dataURI', (dataURI) => {
     // socket.emit('dataURI', dataURI);
     socket.broadcast.emit('dataURI', dataURI);
   });
+
+  // 创建房间
+  socket.on('create', (roomNo) => {
+    // 创建房间，socket没有创建房间这个选项，只有加入房间
+    // 参考：https://stackoverflow.com/questions/19150220/creating-rooms-in-socket-io
+    socket.join(roomNo);
+  });
+
+  // 某个用户加入房间
+  // socket.on('joined', (data) => {
+  //   socket.join(data.roomNo);
+  //   // io.sockets.clients().forEach((a) => {
+  //   //   // .....
+  //   //   console.log(a.id);
+  //   // });
+  //   // 向其它socket发送信息
+  //   // socket.broadcast.emit('joined', (data.user.username));
+  //   // socket.broadcast.to(data.roomNo).emit('joined', (data.user.username));
+  //   socket.to(data.roomNo).emit('joined', data.user.username);
+  //
+  //   socket.on('say to someone', function(id, msg){
+  //     socket.broadcast.to(id).emit('my message', msg);
+  //   });
+  // });
+  socket.on('joined', (data) => {
+    socket.join(data.roomNo);
+    console.log(io.sockets.adapter.rooms);
+    socket.broadcast.to(data.roomNo).emit('joined', data.user);
+  });
+
 
   socket.on('chat', (chatinfo) => {
     socket.emit('chat', chatinfo);
