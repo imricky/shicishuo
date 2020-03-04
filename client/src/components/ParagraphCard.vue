9f083820603b34d747d027fce62a84414b7fbb0d
rqcao <rqcao@outlook.com>
2020-03-04 22:41:08 +0800

    完成每日名句的样式

--- client/src/components/ParagraphCard.vue
+++ client/src/components/ParagraphCard.vue
@@ -2,14 +2,25 @@
   <div class="paragraph-card">
     <el-card class="box-card">
     <div slot="header" class="clearfix">
-      <span>卡片名称</span>
+      <span>每日名句</span>
       <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
     </div>
-      <div style="width: 100%">
-        <el-image src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" fit='fill' class="image"></el-image>
+    <div class="main-card">
+      <el-image
+        class="header-image"
+        src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+        fit="cover"></el-image>
+      <div class="paragraph-info">
+        <div class="one-paragraph">
+<!--          TODO: 诗词移动端自适应，PC端是一行，移动端两行-->
+          宝剑锋从磨，梅花香自苦
+<!--          宝剑锋从磨砺出，梅花香自苦寒来-->
+        </div>
+        <div class="other">
+          <span class="author">—— 杜甫</span>
+          <span class="title">《望岳》</span>
+        </div>
       </div>
-    <div v-for="o in 4" :key="o" class="text item">
-      {{'列表内容 ' + o }}
     </div>
   </el-card>
   </div>
@@ -39,12 +50,12 @@ export default {
     justify-content: center;
     align-items: center;
     max-width: 680px;
-    min-height: 320px;
+    min-height: 200px;
   }
   .box-card {
     /*TODO: 媒体查询，适配不同分辨率*/
     max-width: 680px;
-    min-height: 320px;
+    min-height: 200px;
     /*是这一行导致了移动端css不出来*/
     min-width: 550px;
   }
@@ -55,6 +66,38 @@ export default {
   }
   .clearfix:after {
     clear: both
+  }
+
+  /*主信息*/
+  .main-card{
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+    flex-direction: row;
+    /*头像信息*/
+    .header-image{
+      width: 100px;
+      height: 100px;
+    }
+    /*诗句信息*/
+    .paragraph-info{
+      margin-left: 20px;
+      .one-paragraph{
+        font-size: 24px;
+        font-weight: bold;
+        line-height: 40px;
+      }
+      .other{
+        margin-top: 20px;
+        float: right;
+        .author{
+          color: #606266;
+        }
+        .title{
+          color: #606266;
+        }
+      }
+    }
   }
   /* 横向放置的手机和竖向放置的平板之间的分辨率 */
   @media screen and (max-width: 767px) {
@@ -71,7 +114,7 @@ export default {
     /*}*/
     .box-card {
       max-width: 340px;
-      min-height: 320px;
+      min-height: 220px;
       min-width: 320px;
     }
 
