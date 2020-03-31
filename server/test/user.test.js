6ce3058a6b69dff60b28c6e46c5fa34ac46ca623
rqcao <rqcao@outlook.com>
2020-03-31 23:06:07 +0800

    完成用户收藏和注册的单元测试

--- server/test/user.test.js
+++ server/test/user.test.js
@@ -79,4 +79,107 @@ describe('site/users', () => {
         expect(response.body.data.username).to.be.equal('test11');
       }));
   });
+
+  describe('登录', () => {
+    it('登录时用户名和密码不能为空', () => request(app)
+      .post('/users/login')
+      .send({
+        username: '',
+        password,
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response) => {
+        console.log(response.body);
+        expect(response.body.code).to.be.equal(401);
+        expect(response.body.msg).to.be.equal('登录失败，用户名或密码错误');
+      }));
+    it('登录失败,密码不对', () => request(app)
+      .post('/users/login')
+      .send({
+        username: 'crq1',
+        password: '123',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response) => {
+        console.log(response.body);
+        expect(response.body.code).to.be.equal(401);
+        expect(response.body.msg).to.be.equal('登录失败，用户名或密码错误');
+      }));
+    it('登录成功', () => request(app)
+      .post('/users/login')
+      .send({
+        username: 'crq1',
+        password: '123456',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response) => {
+        console.log(response.body);
+        expect(response.body.code).to.be.equal(200);
+        expect(response.body.msg).to.be.equal('登录成功');
+        expect(response.body.data.username).to.be.equal('crq1');
+      }));
+  });
+
+  describe('用户收藏', () => {
+    it('收藏的时候乱传信息', () => request(app)
+      .post('/users/collect')
+      .send({
+        username: '',
+        password,
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body.code).to.be.equal(500);
+      }));
+
+    it('收藏传入userid为空', () => request(app)
+      .post('/users/collect')
+      .send({
+        userid: '',
+        poemid: '123',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body.code).to.be.equal(500);
+      }));
+
+    it('收藏成功', () => request(app)
+      .post('/users/collect')
+      .send({
+        userid: '5e4ba2ae6806648a37b134fe',
+        poemid: '5e33e18f13e2baa51a55d8e2',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response) => {
+        console.log(response.body);
+        expect(response.body.code).to.be.equal(200);
+        expect(response.body.data.nModified).to.be.equal(1);
+      }));
+    it('重复收藏', () => request(app)
+      .post('/users/collect')
+      .send({
+        userid: '5e4ba2ae6806648a37b134fe',
+        poemid: '5e33e18f13e2baa51a55d8e2',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response) => {
+        console.log(response.body);
+        expect(response.body.code).to.be.equal(200);
+        expect(response.body.data.nModified).to.be.equal(0);
+      }));
+  });
 });
